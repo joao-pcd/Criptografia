afeae3b8166daffaea78d249890a1534c796f15d
joao-pcd <jclementinodias@gmail.com>
2024-07-23 20:06:59 -0300

    Adição do @Table e @EqualsAndHashCode na classe User e remoção do @PrePersist e @PreUpdate

--- src/main/java/com/joaopcd/criptografia/domain/model/User.java
+++ src/main/java/com/joaopcd/criptografia/domain/model/User.java
@@ -1,19 +1,18 @@
 package com.joaopcd.criptografia.domain.model;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.util.HashSet;
 import java.util.Set;
 
+@EqualsAndHashCode
 @AllArgsConstructor
 @NoArgsConstructor
 @Setter
 @Getter
-@Entity(name = "tb_user")
+@Entity
+@Table(name = "tb_user")
 public class User {
 
     @Id
@@ -25,14 +24,4 @@ public class User {
     @OneToMany(mappedBy = "userId", cascade = CascadeType.PERSIST, orphanRemoval = true)
     private Set<Card> cards = new HashSet<>();
 
-    @PrePersist
-    @PreUpdate
-    private void prePersist() {
-        cards.forEach(card -> card.setUserId(this.id));
-    }
-
-    public void addCard(Card card){
-        card.setUserId(this.id);
-        this.cards.add(card);
-    }
 }
